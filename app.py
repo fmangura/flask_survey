77a18d494238295b8fd8d50cacef1fd749ab1119
Franz Nicole Manguray <franzm@Franzs-MacBook-Pro.local>
2023-11-07 23:36:50 -0800

    Step 8

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import Survey, satisfaction_siurvey
 
-responses = []
+# responses = []
 questions = [ 
     'Question1',
     'Question2',
@@ -21,25 +21,40 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 def root_page():
     return render_template('homepage.html')
 
-@app.route('/question/<int:number>')
+@app.route('/question-session/<int:number>')
 def first_question(number):
-    if len(responses) != len(questions):
+
+    if len(session['response']) != len(questions):
         return render_template('question.html', question=questions[number])
     
-    if len(responses) != number:
+    if len(session['response']) != number:
         flash("You are trying to access an invalid questions")
-        return redirect(f'/question/{len(responses)}')
+        return redirect(f"/question/{len(session['response'])}")
     
-    if len(responses) == len(questions):
+    if len(session['response']) == len(questions):
         return redirect('/complete')
     
 
 @app.route('/question/answer', methods=['POST'])
 def answer_question():
     answer = request.form['answer']
-    responses.append(answer)
-    return redirect(f'/question/{len(responses)}')
+    print(answer)
+    response = session['response']
+    print(response)
+    response.append(answer)
+    print(response)
+    session['response'] = response
+    print(len(session['response']))
+    print(session['response'])
+    return redirect(f"/question-session/{len(session['response'])}")
 
 @app.route('/complete')
 def complete():
-   return render_template('complete.html', question=questions, response=responses)
+   return render_template('complete.html')
+
+@app.route('/question-session', methods=['POST'])
+def start_session():
+    session["response"] = []
+    print(len(session['response']))
+    return redirect('/question-session/0')
+
